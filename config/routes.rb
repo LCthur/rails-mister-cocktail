1e94fe6dbdff74b71ba3059d73698fd3b52e7db5
Loic Thurre <loic.thurre@gmail.com>
2019-05-16 11:51:07 +0200

    cocktails controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'cocktails/home'
+  get 'cocktails/show'
+  get 'cocktails/new'
+  get 'cocktails/create'
+  get 'cocktails/update'
+  get 'cocktails/edit'
+  get 'cocktails/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+   root to: 'cocktails#home'
 end
