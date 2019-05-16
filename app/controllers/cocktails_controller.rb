ca1fbc84ce5abca243a4321cb8fd82cdcb88e0ef
Loic Thurre <loic.thurre@gmail.com>
2019-05-16 14:27:58 +0200

    logic and forms are made

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -8,23 +8,38 @@ class CocktailsController < ApplicationController
     @cocktail = Cocktail.find(params[:id])
   end
 
+  def index
+    @cocktails = Cocktail.all
+  end
+
   def new
     @cocktail = Cocktail.new
   end
 
   def create
-  end
-
-  def update
+    @cocktail = Cocktail.new(cocktail_params)
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
   end
 
   def edit
+  end
 
+  def update
   end
 
   def destroy
     @cocktail = Cocktail.find(params[:id])
     @cocktail.destroy
     redirect_to root_path
+  end
+
+  private
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
   end
 end
